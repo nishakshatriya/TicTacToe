87faaae7e7db0833bda41b8360ded24aae0be1b5
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-16 11:56:53 +0530

    Added: Assigning symbol 'X' or 'O'  for Player

--- tictactoe.sh
+++ tictactoe.sh
@@ -2,9 +2,21 @@
 
 echo "Welcome to TIC-TAC-TOE Game"
 
-for(( position=1 ; position<=9 ; position++ ))
-do
-	gameBoard["$position"]=$position
-done
+function resettingBoard(){
+	gameBoard=( 1 2 3 4 5 6 7 8 9 )
 
-echo ${gameBoard[@]}
+	echo	" ${gameBoard[0]} ${gameBoard[1]} ${gameBoard[2]} "
+	echo 	" ${gameBoard[3]} ${gameBoard[4]} ${gameBoard[5]} "
+	echo 	" ${gameBoard[6]} ${gameBoard[7]} ${gameBoard[8]} "
+}
+
+function gettingSymbols(){
+	resettingBoard
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		echo "Player got 'X' symbol"
+	else
+		echo "Player got 'O' symbol"
+	fi
+}
+gettingSymbols
